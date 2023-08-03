df2daa8d6f9e739151a2f2455d2da728d26b6fbb
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-03 15:38:56 -0700

    add routes

--- pokedex-rails-backend/config/routes.rb
+++ pokedex-rails-backend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+    namespace :api, defaults: { format: :json } do
+        get '/pokemon/types', to: 'pokemon#types', as: :pokemon_types
+        
+        resources :items, only: [:update, :destroy]
+      
+        resources :pokemon, only: [:index, :create, :show, :update] do
+            resources :items, only: [:index, :create]
+        end
+    end
 end
